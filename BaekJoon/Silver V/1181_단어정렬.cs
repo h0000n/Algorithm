6bbf4adf17337dc41b7ac4fc4e2a67a8b1d8df29
h0000n <anjunghoon2716@gmail.com>
2024-10-16 23:44:48 +0900

    add : 1016 Solution
    1016 백준 에센셜 2단계 문제 풀이

---
+++ BaekJoon/Silver V/1181_단어정렬.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        _Sol();
+    }
+
+    static void _Sol()
+    {
+        string input;
+        int Length = int.Parse(Console.ReadLine());
+        string[] inputVar = new string[Length];
+        for (int i = 0; i < Length; i++)
+        {
+            input = Console.ReadLine();
+            inputVar[i] = input;
+        }
+        inputVar = inputVar.Distinct().ToArray();
+        Array.Sort(inputVar);
+        inputVar = inputVar.OrderBy(x => x.Length).ToArray();
+
+        foreach (string result in inputVar)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ BaekJoon/Silver V/2751_수 정렬하기 2.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace sol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _Sol();
+        }
+
+        static void _Sol()
+        {
+            int input;
+            int Length = int.Parse(Console.ReadLine());
+            int[] inputVar = new int[Length];
+            for (int i = 0; i < Length; i++)
+            {
+                input = int.Parse(Console.ReadLine());
+                inputVar[i] = input;
+            }
+            inputVar = inputVar.Distinct().ToArray();
+            Array.Sort(inputVar);
+
+            StringBuilder result = new StringBuilder(String.Join("\n", inputVar));
+
+            Console.WriteLine(result);
+        }
+    }
+}
